cfc31d807162555d9ff0abfb95cb03dbb0b4a8f1
Greg Knapp <virtual.greg@gmail.com>
2011-05-08 00:18:03 +0100

    Fix user ID and last login bugs, add gift related methods

--- app/model/user.php
+++ app/model/user.php
@@ -13,12 +13,12 @@ class Model_User extends Lib_Model {
 		$passCode = $request->getPost('passcode');
 		if (preg_match('/[A-z0-9\s]{6,8}/', $passCode)) {
 			$passCode = strtolower(trim($passCode));
-			$userId = $this->getDB()->query(
+			$user = $this->getDB()->query(
 				"SELECT user_id FROM user WHERE passcode = ?", $passCode
 			)->fetch();
 
-			if ($userId > 0) {
-				$session->userId = $this->userId = $userId;
+			if ($user['user_id'] > 0) {
+				$session->userId = $this->userId = $user['user_id'];
 				$this->_updateLastLogin();
 				return true;
 			}
@@ -64,8 +64,8 @@ class Model_User extends Lib_Model {
 			"SELECT guest_id FROM guest WHERE user_id = ?", $this->userId
 		)->fetchAll();
 
-		//var_dump("SELECT guest_id FROM guest WHERE user_id = " . $this->userId);
-		//var_dump($guests);
+		// var_dump("SELECT guest_id FROM guest WHERE user_id = " . $this->userId);
+		// var_dump($guests);
 
 		if (empty($guests))
 			return false;
@@ -91,9 +91,45 @@ class Model_User extends Lib_Model {
 		return strtoupper($postcode);
 	}
 
+	public function buyGifts($gifts) {
+		foreach ($gifts as $id => $quantity) {
+			if (empty($quantity))
+				continue;
+
+			$this->getDB()->query(
+				"REPLACE INTO user_basket (user_id, gift_id, quantity) " .
+				"VALUES (?, ?, ?)",
+				array($this->userId, $id, $quantity)
+			);
+		}
+	}
+
+	public function getBoughtGifts() {
+		$gifts = $this->getDB()->query(
+			"SELECT g.name, g.price, quantity FROM user_basket ub " .
+			"INNER JOIN gift g ON (g.gift_id = ub.gift_id) " .
+			"WHERE user_id = ?",
+			$this->userId
+		)->fetchAll();
+
+		return array_map(function($gift) {
+			$gift['total'] = $gift['quantity'] * $gift['price'];
+			return $gift;
+		}, $gifts);
+	}
+
+	public function boughtGifts() {
+		$gifts = $this->getDB()->query(
+			"SELECT gift_id FROM user_basket WHERE user_id = ?",
+			$this->userId
+		)->fetch();
+
+		return ($gifts && count($gifts) > 0);
+	}
+
 	protected function _updateLastLogin() {
 		$this->getDB()->query(
-			"UPDATE user SET lastlogin = datetime(now) WHERE user_id = ?",
+			"UPDATE user SET lastlogin = datetime('now') WHERE user_id = ?",
 			$this->userId
 		);
 	}
